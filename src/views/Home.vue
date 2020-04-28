a8460779ee45da3342fb9283ba2448f6f3fe7e94
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-29 00:06:20 +0700

    Добавил компонент AppHeader

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header></app-header>
+    <router-view/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AppHeader from '../components/AppHeader'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    AppHeader
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .home {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+</style>
