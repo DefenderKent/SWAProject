099f2a61f952fd9d6d4104ce84d89f339df17680
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-29 00:05:16 +0700

    Стили для App и подключил reset.scss

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "@/assets/style/reset.scss";
+  html {
+    font-size: 10px;    
+  }
 
-#nav {
-  padding: 30px;
+  #app {
+    height: 100%;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+  @media only screen and (min-width: 1500px) {
+    html {
+      font-size: 8px;
     }
   }
-}
 </style>
